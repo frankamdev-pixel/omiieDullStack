f35228f3b18471163575b8d3da592da459e2938b
Frankandev <fkamgang06@gmail.com>
2025-09-30 15:08:01 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    // return Inertia::render('welcome');
-    return Inertia::render('Test');
+    return Inertia::render('welcome');
+;
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
